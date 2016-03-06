d8dffbdce3cfb807becc804eed26f0a28e428d81
abeatd <abeatd20@gmail.com>
2016-03-06 19:31:21 +0700

    Added files via upload

---
+++ sketch_mar04a.ino
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "SoftwareSerial.h"
+#define SSID ""
+#define PASS ""
+#define IP "184.106.153.149" // thingspeak.com
+String apiKey = "";
+String GET = "GET /update?key=[THINGSPEAK_KEY]&field1=";
+
+const int sensorIn = A0;
+int mVperAmp = 100;
+double Voltage = 0;
+double VRMS = 0;
+double AmpsRMS = 0;
+int ledPin = 13;
+
+SoftwareSerial wifi(6,7);
+
+void setup() 
+{
+ pinMode(ledPin, OUTPUT)
+ wifi.begin(115200)
+ Serial.begin(9600)
+ wifi.println("AT+RST")
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH);   
+  delay(200);               
+  digitalWrite(ledPin, LOW);
+
+
+}
